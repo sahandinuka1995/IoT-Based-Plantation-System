2ad7a90dec92a22e7939999dfad48252445afd33
sahandinuka1995 <sahandinuka1995@gmail.com>
2024-03-17 15:09:16 +0530

    update responses

--- python-backend/app.py
+++ python-backend/app.py
@@ -4,30 +4,45 @@ import pandas as pd
 import requests
 
 app = Flask(__name__)
+
+
 def load_modal():
     model_url = 'https://github.com/sahandinuka1995/IoT-Based-Plantation-System/raw/production/python-backend/model.pkl'
     response = requests.get(model_url)
     if response.status_code == 200:
         return pickle.loads(response.content)
 
+
 @app.get("/")
 def get():
-    return "Plantation flask backend running successfully..."    
+    return "Plantation flask backend running successfully..."
+
 
 @app.post("/prediction")
 def prediction():
-    loaded_model = load_modal()
-    data = request.get_json()
-    new_data = {
-        'N': [data['N']],
-        'P': [data['P']],
-        'K': [data['K']],
-        'temperature': [data['temperature']],
-        'humidity': [data['humidity']],
-        'ph': [data['ph']],
-        'rainfall': [data['rainfall']]
-    }
+    try:
+        loaded_model = load_modal()
+        data = request.get_json()
+        new_data = {
+            'N': [data['N']],
+            'P': [data['P']],
+            'K': [data['K']],
+            'temperature': [data['temperature']],
+            'humidity': [data['humidity']],
+            'ph': [data['ph']],
+            'rainfall': [data['rainfall']]
+        }
 
-    new_data_df = pd.DataFrame(new_data)
-    predicted_label = loaded_model.predict(new_data_df)
-    return predicted_label[0]
+        new_data_df = pd.DataFrame(new_data)
+        predicted_label = loaded_model.predict(new_data_df)
+        return jsonify({
+            "status": 200,
+            "data": predicted_label[0],
+            "message": 'Operation Successful'
+        })
+    except:
+        return jsonify({
+            "status": 400,
+            "data": null,
+            "message": 'Something went wrong'
+        })

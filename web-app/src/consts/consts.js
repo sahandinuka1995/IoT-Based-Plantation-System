750106c83055dfe0a6d7ea925ad77b731ccd7156
sahandinuka1995 <sahandinuka1995@gmail.com>
2024-04-10 23:44:55 +0530

    modify prediction details

--- web-app/src/consts/consts.js
+++ web-app/src/consts/consts.js
@@ -1,3 +1,17 @@
+import imgBeans from '../assets/images/icons/plants/icons8-kidney-beans-96.png'
+import imgMungBeans from '../assets/images/icons/plants/icons8-mung-bean-100.png'
+import imgPomegranate from '../assets/images/icons/plants/icons8-pomegrande-96.png'
+import imgBanana from '../assets/images/icons/plants/icons8-banana-96.png'
+import imgRice from '../assets/images/icons/plants/icons8-rice-48.png'
+import imgMaize from '../assets/images/icons/plants/icons8-maize-96.png'
+import imgMango from '../assets/images/icons/plants/icons8-mango-96.png'
+import imgJackfruit from '../assets/images/icons/plants/icons8-jackfruit-64.png'
+import imgWatermelon from '../assets/images/icons/plants/icons8-watermelon-100.png'
+import imgOrange from '../assets/images/icons/plants/icons8-orange-64.png'
+import imgPapaya from '../assets/images/icons/plants/icons8-papaya-96.png'
+import imgCoconut from '../assets/images/icons/plants/icons8-coconut-96.png'
+import imgCoffee from '../assets/images/icons/plants/icons8-coffee-96.png'
+
 export const plansResultSteps = {
     FIND: 'FIND',
     RESULT: 'RESULT'
@@ -17,3 +31,19 @@ export const USER_ROLES_DROPDOWN = [
     {label: 'Admin', value: USER_ROLES.ADMIN},
     {label: 'User', value: USER_ROLES.USER}
 ]
+
+export const PLANT_IMG_LIST = {
+    beans: imgBeans,
+    mungbean: imgMungBeans,
+    pomegranate: imgPomegranate,
+    banana: imgBanana,
+    rice: imgRice,
+    maize: imgMaize,
+    mango: imgMango,
+    jackfruit: imgJackfruit,
+    watermelon: imgWatermelon,
+    orange: imgOrange,
+    papaya: imgPapaya,
+    coconut: imgCoconut,
+    coffee: imgCoffee
+}

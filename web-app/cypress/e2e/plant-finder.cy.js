db15fb50367048c5c961351ee251f442d985aea2
sahandinuka1995 <sahandinuka1995@gmail.com>
2024-04-25 15:54:58 +0530

    JMeter testing and update report

--- web-app/cypress/e2e/plant-finder.cy.js
+++ web-app/cypress/e2e/plant-finder.cy.js
@@ -1,4 +1,15 @@
 describe('template spec', () => {
+  it('Login', () => {
+    cy.visit('https://iot-based-plantation-system-2evs.onrender.com/login')
+
+    cy.get('#login-username').type('sahan')
+    cy.get('#login-password').type('1234')
+    cy.get('.btn').click()
+
+    cy.url().should('include', '/home')
+    cy.get('.navbar-container').should('be.visible')
+  })
+
   it('passes', () => {
     cy.visit('https://iot-based-plantation-system-2evs.onrender.com/plant-finder')
   })

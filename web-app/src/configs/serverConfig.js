52851cfb7d6ecbb6cbdd3fc07f362f7835d7f03f
sahandinuka1995 <sahandinuka1995@gmail.com>
2024-05-07 22:58:22 +0530

    change serverConfig.js

--- web-app/src/configs/serverConfig.js
+++ web-app/src/configs/serverConfig.js
@@ -1,8 +1,8 @@
-const PROD = process.env.SERVER_TYPE === 'PROD'
-console.log(PROD, process.env.SERVER_TYPE)
-const SERVER_URL = PROD ? process.env.SERVER_HOST : 'http://localhost:4000'
+const PROD = window.location.host === 'https://iot-based-plantation-system-2evs.onrender.com'
+
+const SERVER_URL = PROD ? 'https://iot-based-plantation-system-nodejs.onrender.com' : 'http://localhost:4000'
 const VERSION = "/api/v1"
-console.log(SERVER_URL)
+
 export const SERVER_PATH = {
     SERVER_URL, VERSION
 }

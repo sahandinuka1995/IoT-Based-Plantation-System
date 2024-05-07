27a6a52ed276258e2e8ad71cefb70570a9e02043
sahandinuka1995 <sahandinuka1995@gmail.com>
2024-05-07 12:31:34 +0530

    change MySQL connection config

--- nodejs-backend/config/db.js
+++ nodejs-backend/config/db.js
@@ -5,8 +5,6 @@ const config = {
     database: process.env.DATABASE_NAME.trim(),
     waitForConnections: true,
     connectionLimit: 10,
-    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
     queueLimit: 0,
     enableKeepAlive: true,
     keepAliveInitialDelay: 0
